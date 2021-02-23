1ba0051618e17d866ad84ac65df8c6d43393b2b2
Sercan Virlan <sv@cortezgroupe.com>
2021-02-23 20:34:19 +0300

    feat: sqlite coverrage

---
+++ test/scripts/mssql-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wait_time=15s
+password=Test@123
+
+# wait for SQL Server to come up
+echo importing data will start in $wait_time...
+sleep $wait_time
+echo importing data...
+
+# run the init script to create the DB and the tables in /table
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./init-mssql.sql
+
+for entry in "./mssql.sql"
+do
+  echo executing $entry
+  /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i $entry
+done
---
+++ test/sqlite.spec.ts
@@ -0,0 +1,484 @@
+import Knex from 'knex';
+import { expect } from 'chai';
+import schemaInspector from '../lib';
+import { SchemaInspector } from '../lib/types/schema-inspector';
+
+describe('sqlite', () => {
+  let database: Knex;
+  let inspector: SchemaInspector;
+
+  before(() => {
+    database = Knex({
+      client: 'sqlite3',
+      connection: {
+        filename: './test/db/sqlite.db',
+      },
+    });
+    inspector = schemaInspector(database);
+  });
+
+  after(async () => {
+    await database.destroy();
+  });
+
+  describe('.tables', () => {
+    it('returns tables', async () => {
+      expect(await inspector.tables()).to.deep.equal([
+        'page_visits',
+        'teams',
+        'users',
+      ]);
+    });
+  });
+
+  describe('.tableInfo', () => {
+    it('returns information for all tables', async () => {
+      expect(await inspector.tableInfo()).to.deep.equal([
+        {
+          name: 'page_visits',
+          sql:
+            'CREATE TABLE page_visits (\n' +
+            '  request_path varchar(100)\n' +
+            ',  user_agent varchar(200)\n' +
+            ',  created_at datetime\n' +
+            ')',
+        },
+        {
+          name: 'teams',
+          sql:
+            'CREATE TABLE "teams" (\n' +
+            '\t"id"\tINTEGER NOT NULL,\n' +
+            '\t"uuid"\tvarchar(36) NOT NULL UNIQUE,\n' +
+            '\t"name"\tvarchar(100),\n' +
+            '\t"description"\ttext,\n' +
+            '\t"credits"\tinteger,\n' +
+            '\t"created_at"\tdatetime,\n' +
+            '\t"activated_at"\tdate,\n' +
+            '\tPRIMARY KEY("id" AUTOINCREMENT)\n' +
+            ')',
+        },
+        {
+          name: 'users',
+          sql:
+            'CREATE TABLE "users" (\n' +
+            '\t"id"\tINTEGER NOT NULL,\n' +
+            '\t"team_id"\tinteger NOT NULL,\n' +
+            '\t"email"\tvarchar(100),\n' +
+            '\t"password"\tvarchar(60),\n' +
+            '\tPRIMARY KEY("id" AUTOINCREMENT),\n' +
+            '\tFOREIGN KEY("team_id") REFERENCES "teams"("id")\n' +
+            ')',
+        },
+      ]);
+    });
+
+    it('returns information for specific table', async () => {
+      expect(await inspector.tableInfo('teams')).to.deep.equal({
+        name: 'teams',
+        sql:
+          'CREATE TABLE "teams" (\n' +
+          '\t"id"\tINTEGER NOT NULL,\n' +
+          '\t"uuid"\tvarchar(36) NOT NULL UNIQUE,\n' +
+          '\t"name"\tvarchar(100),\n' +
+          '\t"description"\ttext,\n' +
+          '\t"credits"\tinteger,\n' +
+          '\t"created_at"\tdatetime,\n' +
+          '\t"activated_at"\tdate,\n' +
+          '\tPRIMARY KEY("id" AUTOINCREMENT)\n' +
+          ')',
+      });
+    });
+  });
+
+  describe('.hasTable', () => {
+    it('returns if table exists or not', async () => {
+      expect(await inspector.hasTable('teams')).to.equal(true);
+      expect(await inspector.hasTable('foobar')).to.equal(false);
+    });
+  });
+
+  describe('.columns', () => {
+    it('returns information for all tables', async () => {
+      expect(await inspector.columns()).to.deep.equal([
+        { table: 'page_visits', column: 'request_path' },
+        { table: 'page_visits', column: 'user_agent' },
+        { table: 'page_visits', column: 'created_at' },
+        { table: 'teams', column: 'id' },
+        { table: 'teams', column: 'uuid' },
+        { table: 'teams', column: 'name' },
+        { table: 'teams', column: 'description' },
+        { table: 'teams', column: 'credits' },
+        { table: 'teams', column: 'created_at' },
+        { table: 'teams', column: 'activated_at' },
+        { table: 'users', column: 'id' },
+        { table: 'users', column: 'team_id' },
+        { table: 'users', column: 'email' },
+        { table: 'users', column: 'password' },
+      ]);
+    });
+
+    it('returns information for specific table', async () => {
+      expect(await inspector.columns('teams')).to.deep.equal([
+        { column: 'id', table: 'teams' },
+        { column: 'uuid', table: 'teams' },
+        { column: 'name', table: 'teams' },
+        { column: 'description', table: 'teams' },
+        { column: 'credits', table: 'teams' },
+        { column: 'created_at', table: 'teams' },
+        { column: 'activated_at', table: 'teams' },
+      ]);
+    });
+  });
+
+  describe('.columnInfo', () => {
+    it('returns information for all columns in all tables', async () => {
+      expect(await inspector.columnInfo()).to.deep.equal([
+        {
+          name: 'request_path',
+          table: 'page_visits',
+          type: 'varchar',
+          default_value: null,
+          max_length: 100,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'user_agent',
+          table: 'page_visits',
+          type: 'varchar',
+          default_value: null,
+          max_length: 200,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'created_at',
+          table: 'page_visits',
+          type: 'datetime',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'id',
+          table: 'teams',
+          type: 'integer',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: false,
+          is_unique: false,
+          is_primary_key: true,
+          has_auto_increment: true,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'uuid',
+          table: 'teams',
+          type: 'varchar',
+          default_value: null,
+          max_length: 36,
+          precision: null,
+          scale: null,
+          is_nullable: false,
+          is_unique: true,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'name',
+          table: 'teams',
+          type: 'varchar',
+          default_value: null,
+          max_length: 100,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'description',
+          table: 'teams',
+          type: 'text',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'credits',
+          table: 'teams',
+          type: 'integer',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'created_at',
+          table: 'teams',
+          type: 'datetime',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'activated_at',
+          table: 'teams',
+          type: 'date',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'id',
+          table: 'users',
+          type: 'integer',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: false,
+          is_unique: false,
+          is_primary_key: true,
+          has_auto_increment: true,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'team_id',
+          table: 'users',
+          type: 'integer',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: false,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: 'id',
+          foreign_key_table: 'teams',
+        },
+        {
+          name: 'email',
+          table: 'users',
+          type: 'varchar',
+          default_value: null,
+          max_length: 100,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'password',
+          table: 'users',
+          type: 'varchar',
+          default_value: null,
+          max_length: 60,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+      ]);
+    });
+
+    it('returns information for all columns in specific table', async () => {
+      expect(await inspector.columnInfo('teams')).to.deep.equal([
+        {
+          name: 'id',
+          table: 'teams',
+          type: 'integer',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: false,
+          is_unique: false,
+          is_primary_key: true,
+          has_auto_increment: true,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'uuid',
+          table: 'teams',
+          type: 'varchar',
+          default_value: null,
+          max_length: 36,
+          precision: null,
+          scale: null,
+          is_nullable: false,
+          is_unique: true,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'name',
+          table: 'teams',
+          type: 'varchar',
+          default_value: null,
+          max_length: 100,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'description',
+          table: 'teams',
+          type: 'text',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'credits',
+          table: 'teams',
+          type: 'integer',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'created_at',
+          table: 'teams',
+          type: 'datetime',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+        {
+          name: 'activated_at',
+          table: 'teams',
+          type: 'date',
+          default_value: null,
+          max_length: null,
+          precision: null,
+          scale: null,
+          is_nullable: true,
+          is_unique: false,
+          is_primary_key: false,
+          has_auto_increment: false,
+          foreign_key_column: null,
+          foreign_key_table: null,
+        },
+      ]);
+    });
+
+    it('returns information for a specific column in a specific table', async () => {
+      expect(await inspector.columnInfo('teams', 'uuid')).to.deep.equal({
+        name: 'uuid',
+        table: 'teams',
+        type: 'varchar',
+        default_value: null,
+        max_length: 36,
+        precision: null,
+        scale: null,
+        is_nullable: false,
+        is_unique: true,
+        is_primary_key: false,
+        has_auto_increment: false,
+        foreign_key_column: null,
+        foreign_key_table: null,
+      });
+    });
+  });
+
+  describe('.primary', () => {
+    it('returns primary key for a table', async () => {
+      expect(await inspector.primary('teams')).to.equal('id');
+      expect(await inspector.primary('page_visits')).to.equal(null);
+    });
+  });
+});
