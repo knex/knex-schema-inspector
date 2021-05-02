6b6029eb24740e0763c7566eecf098635c68044b
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-05-02 17:33:16 +0200

    Added back MSSQL 15 second wait

--- test/scripts/mssql-entrypoint.sh
+++ test/scripts/mssql-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+wait_time=15s
 password=Test@123
 
 # bring up the server
-/opt/mssql/bin/sqlservr
+/opt/mssql/bin/sqlservr &
+
+# wait for SQL Server to come up
+echo importing data will start in $wait_time...
+sleep $wait_time
 
 # run the init script to create the DB and the tables in /table
 echo importing data...
