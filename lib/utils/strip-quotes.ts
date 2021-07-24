e2c810439b24d5ec96f23e99b177bf113bfaefae
Aiden Foxx <aiden.foxx@sbab.se>
2021-07-24 17:31:19 +0200

    Fixed typescript implicit type complaint

--- lib/utils/strip-quotes.ts
+++ lib/utils/strip-quotes.ts
@@ -1,11 +1,11 @@
-export function stripQuotes(value) {
+export function stripQuotes(value?: string | undefined) {
   if (value?.startsWith(`'`) && value?.endsWith(`'`)) {
     return value.slice(1, -1);
   }
   return value;
 }
 
-export function stripDoubleQuotes(value) {
+export function stripDoubleQuotes(value?: string | undefined) {
   if (value?.startsWith('"') && value?.endsWith('"')) {
     return value.slice(1, -1);
   }
