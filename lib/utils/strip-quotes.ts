a310caf76e342ab4f0a5c3694af47ac913e5d824
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-07-24 17:37:34 +0200

    Whoops! That should have been nullable

--- lib/utils/strip-quotes.ts
+++ lib/utils/strip-quotes.ts
@@ -1,11 +1,11 @@
-export function stripQuotes(value?: string | undefined) {
+export function stripQuotes(value?: string | null) {
   if (value?.startsWith(`'`) && value?.endsWith(`'`)) {
     return value.slice(1, -1);
   }
   return value;
 }
 
-export function stripDoubleQuotes(value?: string | undefined) {
+export function stripDoubleQuotes(value?: string | null) {
   if (value?.startsWith('"') && value?.endsWith('"')) {
     return value.slice(1, -1);
   }
