fac60abbe8e2e97bc2cd49b1e335c55b49d9d17f
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-07-25 09:55:46 +0200

    Update strip-quotes.ts

--- lib/utils/strip-quotes.ts
+++ lib/utils/strip-quotes.ts
@@ -1,12 +1,5 @@
 export function stripQuotes(value?: string | null) {
-  if (value?.startsWith(`'`) && value?.endsWith(`'`)) {
-    return value.slice(1, -1);
-  }
-  return value;
-}
-
-export function stripDoubleQuotes(value?: string | null) {
-  if (value?.startsWith('"') && value?.endsWith('"')) {
+  if ((value?.startsWith(`'`) && value?.endsWith(`'`)) || (value?.startsWith('"') && value?.endsWith('"'))) {
     return value.slice(1, -1);
   }
   return value;
