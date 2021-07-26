7b44254137a07a7e3b8b2d6ee03866569c0c24bf
Aiden Foxx <aiden.foxx.mail@gmail.com>
2021-07-26 08:21:55 +0200

    Made prettier happy

--- lib/utils/strip-quotes.ts
+++ lib/utils/strip-quotes.ts
@@ -1,5 +1,8 @@
 export function stripQuotes(value?: string | null) {
-  if ((value?.startsWith(`'`) && value?.endsWith(`'`)) || (value?.startsWith('"') && value?.endsWith('"'))) {
+  if (
+    (value?.startsWith(`'`) && value?.endsWith(`'`)) ||
+    (value?.startsWith('"') && value?.endsWith('"'))
+  ) {
     return value.slice(1, -1);
   }
   return value;
