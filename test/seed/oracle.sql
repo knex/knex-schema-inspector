09fccaa53d8e2162a8f77dce96deea6453682a30
Oreilles <oreilles.github@nitoref.io>
2021-11-05 19:11:58 +0100

    Revert "Fix composite indexes on Oracle" and remove failing on Oracle.
    This reverts commit 691a385bf42f9cf89c2a4b53372bfad1cf8c3aa5.

--- test/seed/oracle.sql
+++ test/seed/oracle.sql
@@ -33,9 +33,7 @@ create table users (
   constraint fk_team_id
     foreign key (team_id)
     references teams (id)
-    on delete cascade,
-  constraint team_id_email_unique
-    unique (team_id, email)
+    on delete cascade
 );
 
 -- Generate ID using sequence and trigger
