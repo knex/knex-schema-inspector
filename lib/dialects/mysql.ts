9257c428527678ceac15f292fc3c4aca15393112
Oreilles <oreilles.github@nitoref.io>
2021-11-26 12:33:44 +0100

    Fix MySQL `tinyint(1) not being treated as boolean

--- lib/dialects/mysql.ts
+++ lib/dialects/mysql.ts
@@ -34,18 +34,9 @@ type RawColumn = {
 };
 
 export function rawColumnToColumn(rawColumn: RawColumn): Column {
-  let dataType = rawColumn.COLUMN_TYPE.split('(')[0];
-
-  /**
-   * Smooth out a difference between MySQL and MariaDB. MySQL reports the column type as `int
-   * unsigned`, while MariaDB reports it as `int(11) unsigned`. This would cause the `unsigned` part
-   * of the type to be dropped in the columnInfo retrieval for MariaDB powered databases.
-   */
-  if (
-    rawColumn.COLUMN_TYPE.includes('unsigned') &&
-    dataType.includes('unsigned') === false
-  ) {
-    dataType += ' unsigned';
+  let dataType = rawColumn.COLUMN_TYPE.replace(/\(.*?\)/, '');
+  if (rawColumn.COLUMN_TYPE.startsWith('tinyint(1)')) {
+    dataType = 'boolean';
   }
 
   return {
