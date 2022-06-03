d619f47a34ea6fe4e42390dea09c7ca51a41e14a
Aiden Foxx <aiden.foxx@sbab.se>
2022-06-03 12:39:58 +0200

    Minor tweaks

--- lib/utils/parse-data-default.ts
+++ lib/utils/parse-data-default.ts
@@ -2,13 +2,13 @@
  * Strip leading/trailing quotes from a string and handle null values.
  */
 export function parseDataDefault(value: string | null): string | null {
-  if (value == null) {
+  if (value === null) {
     return null;
   }
 
   const trimmed = value.trim();
 
-  // Oracle can return unquoted string null to indicate null default
+  // Oracle/SQLite can return unquoted string null to indicate null default
   if (trimmed.toLocaleLowerCase() === 'null') {
     return null;
   }
